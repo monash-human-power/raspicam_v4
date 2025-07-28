30d71b6ac1a14db0e18ff802e81ac981cd45de71
Arosh Heenkenda <arosh.heenkenda@outlook.com>
2025-07-28 10:38:43 +1000

    Add V4 HAL.

--- hardware/hal.py
+++ hardware/hal.py
@@ -107,11 +107,47 @@ class V3HAL(HardwareAbstractionLayer):
         return self._battery_adc
 
 
+class V4HAL(HardwareAbstractionLayer):
+    # TODO: Chnge the board pins for V4 displays when ready
+    def __init__(self):
+        self._display_power_led = init_led(17)  # board pin 11, green led
+        self._mqtt_connected_led = init_led(18)  # board pin 24, yellow led
+        self._logging_led = init_led(27)  # board pin 13, red led
+        self._logging_button = init_switch(5, PullUpDown.DOWN)  # board pin 29
+        self._display_power_switch = init_switch(22)  # board pin 15
+        self._battery_adc = init_v3_battery_adc()
+
+    @property
+    def display_power_led(self) -> LED:
+        return self._display_power_led
+
+    @property
+    def mqtt_connected_led(self) -> LED:
+        return self._mqtt_connected_led
+
+    @property
+    def logging_led(self) -> LED:
+        return self._logging_led
+
+    @property
+    def logging_button(self) -> Switch:
+        return self._logging_button
+
+    @property
+    def display_power_switch(self) -> Switch:
+        return self._display_power_switch
+
+    @property
+    def battery_adc(self) -> ADC:
+        return self._battery_adc
+
 def get_hal(bike_str: str) -> HardwareAbstractionLayer:
     if bike_str.lower() == "v2":
         return V2HAL()
     elif bike_str.lower() == "v3":
         return V3HAL()
+    elif bike_str.lower() == "v4":
+        return V4HAL()
     else:
         raise ValueError(
             f"Unknown bike {bike_str}. Unable to initialise hardware."
